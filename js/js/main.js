03beefe91714819546cb950320b2a1726ec85dda
phpsohel <engrsohel123@gmail.com>
2022-12-04 15:47:55 +0600

    Larave Documentation

---
+++ js/js/main.js
@@ -0,0 +1,10 @@
+function length() {
+    let ll = "length Count";
+    alert(ll.length);
+}
+let STringObject = "Apple, Banana, Kiwi";
+console.log(STringObject.slice(7, 13));
+
+let str = "i love joruina";
+let change = str.replace('joruina', 'tamima');
+console.log(change);
